7c6708817646288262aa7b69fe1cd4869be800b6
Vasyl96 <vg.createam@gmail.com>
2022-08-26 18:31:04 +0300

    Зробив слайдер

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -4,7 +4,9 @@ var swiper = new Swiper(".mySwiper", {
                 pagination: {
                     el: ".swiper-pagination",
                     clickable: true,
-                }, 
+                    // Dinamic bullet
+                    dinamicBullets: true,
+                    }, 
                 navigation: {
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev'
