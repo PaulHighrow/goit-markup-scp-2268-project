0a74a495dad8b88f580564407048ea514939c46e
Vasyl96 <vg.createam@gmail.com>
2022-08-26 17:56:26 +0300

    Доробляю слайдер

---
+++ src/js/swiper.js
@@ -0,0 +1,31 @@
+// Swaper start
+
+var swiper = new Swiper(".mySwiper", {
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                }, 
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                },
+        });
+
+// new Swiper('.image-slider', {
+//     // Navigation
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     },
+
+//     // Pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//         // Bullet
+//         clickable: true,
+//         // Dinamic bullet
+//         dinamicBullets: true, 
+//     },
+// });
+
+// Swaper end
