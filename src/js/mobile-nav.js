f7138bd57ab01f5171e5261295647fea0102c693
Kyrylo Sulihan <paulhighrow@gmail.com>
2022-08-27 19:25:54 +0300

    Скрипт мобільної навігації, вирішена проблема свайперу

---
+++ src/js/mobile-nav.js
@@ -0,0 +1,12 @@
+(() => {
+    const navOpenBtn = document.querySelector("[open-button]");
+    const navCloseBtn = document.querySelector("[close-button]");
+    const navMobile = document.querySelector("[nav-mobile]");
+
+    navOpenBtn.addEventListener('click', toggleNav);
+    navCloseBtn.addEventListener('click', toggleNav);
+
+    function toggleNav() {
+    navMobile.classList.toggle('is-hidden');
+  }
+})();
