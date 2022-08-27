4a97e68bcb7a932dc4d92f1246e1402bd599ce9c
PaulHighrow <106202828+PaulHighrow@users.noreply.github.com>
2022-08-27 16:26:25 +0000

    Deploying to gh-pages from @ PaulHighrow/goit-markup-scp-2268-project@f7138bd57ab01f5171e5261295647fea0102c693 🚀

---
+++ index.c9403352.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[open-button]"),t=document.querySelector("[close-button]"),n=document.querySelector("[nav-mobile]");function c(){n.classList.toggle("is-hidden")}e.addEventListener("click",c),t.addEventListener("click",c)}();
+//# sourceMappingURL=index.c9403352.js.map
