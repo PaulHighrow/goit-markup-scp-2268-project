22fb4521cee3aad016c649f8e8b7d7c4cb1c1c87
PaulHighrow <106202828+PaulHighrow@users.noreply.github.com>
2022-08-27 12:52:20 +0000

    Deploying to gh-pages from @ PaulHighrow/goit-markup-scp-2268-project@342173594a12a72737bc5b62946e11a076b8e27b 🚀

--- index.fb0f2716.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0,dinamicBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.fb0f2716.js.map
