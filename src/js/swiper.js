94008a633003c0a278686246830932e8750c80e8
Vasyl96 <vg.createam@gmail.com>
2022-08-27 13:23:59 +0300

    ваивк

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,12 +1,12 @@
 // Swiper start
 
 var swiper = new Swiper(".mySwiper", {
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
-                    // Dinamic bullet
-                    dinamicBullets: true,
-                    }, 
+                // pagination: {
+                //     el: ".swiper-pagination",
+                //     clickable: true,
+                //     // Dinamic bullet
+                //     dinamicBullets: true,
+                //     }, 
                 navigation: {
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev'
