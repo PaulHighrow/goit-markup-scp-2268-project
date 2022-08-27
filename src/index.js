0c1edf075ec9b21f22a4943ce17cd3472d72f5f7
Kyrylo Sulihan <paulhighrow@gmail.com>
2022-08-27 22:46:06 +0300

    Злиття скриптів, фікс багу в хедері

--- src/index.js
+++ src/index.js
@@ -5,14 +5,27 @@
 */
 
 (() => {
-    const navOpenBtn = document.querySelector("[open-button]");
-    const navCloseBtn = document.querySelector("[close-button]");
-    const navMobile = document.querySelector("[nav-mobile]");
+  const navOpenBtn = document.querySelector('[open-button]');
+  const navCloseBtn = document.querySelector('[close-button]');
+  const navMobile = document.querySelector('[nav-mobile]');
 
-    navOpenBtn.addEventListener('click', toggleNav);
-    navCloseBtn.addEventListener('click', toggleNav);
+  navOpenBtn.addEventListener('click', toggleNav);
+  navCloseBtn.addEventListener('click', toggleNav);
 
-    function toggleNav() {
+  function toggleNav() {
     navMobile.classList.toggle('is-open');
   }
 })();
+
+/*
+  |==============================
+  | Swiper script
+  |==============================
+*/
+
+var swiper = new Swiper('.mySwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
