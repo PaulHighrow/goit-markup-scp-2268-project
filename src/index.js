8bbefced1a127c149743219e0ed306281a6baa49
Kyrylo Sulihan <paulhighrow@gmail.com>
2022-08-28 04:50:43 +0300

    Модальне вікно, форма, бекдроп

--- src/index.js
+++ src/index.js
@@ -17,6 +17,42 @@
   }
 })();
 
+
+/*
+  |==============================
+  | Modal window script
+  |==============================
+*/
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[modal-open]'),
+    closeModalBtn: document.querySelector('[modal-close]'),
+    modal: document.querySelector('[modal]'),
+    backdrop: document.querySelector('[backdrop]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  // Добавлена возможность закрытия модального окна по нажатию кнопки Escape
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      refs.modal.classList.add('modal--is-hidden'), refs.backdrop.classList.add('backdrop--is-hidden'), refs.body.classList.remove('no-scroll');
+    }
+  });
+
+  refs.backdrop.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('modal--is-hidden');
+    refs.backdrop.classList.toggle('backdrop--is-hidden');
+    // Отключение скролла при вызове модальной формы
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+
 /*
   |==============================
   | Swiper script
