a5cce70f039348f188ce45587101bad4218e9290
Kyrylo Sulihan <paulhighrow@gmail.com>
2022-08-28 14:01:27 +0300

    Дороблено скрипт модального вікна

--- src/index.js
+++ src/index.js
@@ -25,24 +25,27 @@
 */
 
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[modal-open]'),
-    closeModalBtn: document.querySelector('[modal-close]'),
-    modal: document.querySelector('[modal]'),
-    backdrop: document.querySelector('[backdrop]'),
+  var refs = {
+    openModalBtns: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('[data-backdrop]'),
     body: document.querySelector('body'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtns.forEach(function (open) {
+    open.addEventListener('click', toggleModal);
+  });
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  // Добавлена возможность закрытия модального окна по нажатию кнопки Escape
+  // Press Escape to close
   document.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
       refs.modal.classList.add('modal--is-hidden'), refs.backdrop.classList.add('backdrop--is-hidden'), refs.body.classList.remove('no-scroll');
     }
   });
 
+  // Click into backdrop to close
   refs.backdrop.addEventListener('click', toggleModal);
 
   function toggleModal() {
