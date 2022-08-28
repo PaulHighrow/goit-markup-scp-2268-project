0f2d2a6ff044d0098d9d2b6784d0e4943ba69b1f
PaulHighrow <106202828+PaulHighrow@users.noreply.github.com>
2022-08-28 01:51:14 +0000

    Deploying to gh-pages from @ PaulHighrow/goit-markup-scp-2268-project@8bbefced1a127c149743219e0ed306281a6baa49 🚀

--- index.af587b32.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[open-button]"),t=document.querySelector("[close-button]"),n=document.querySelector("[nav-mobile]");function o(){n.classList.toggle("is-open")}e.addEventListener("click",o),t.addEventListener("click",o)})();new Swiper(".mySwiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.af587b32.js.map
